de0298c6e8799514835cff2961e2b7d54a25ef3f
stevo15025 <stevo15025@gmail.com>
2014-10-21 20:36:33 -0400

    update all

--- Inclass.R
+++ Inclass.R
@@ -16,6 +16,21 @@ abline(mean(daysbelow32[ghog=="Shadow"]),0)
 # make color points
 points(Year[ghog=="NOShadow"],daysbelow32[ghog=="NOShadow"],col="red",pch=4)
 
+mean.b32.ns <- mean(daysbelow32[ghog=="NOShadow"])
+mean.b32.s <- mean(daysbelow32[ghog=="Shadow"])
+sd.b32.ns <- sd(daysbelow32[ghog=="NOShadow"])
+sd.b32.s <- sd(daysbelow32[ghog=="Shadow"])
+ln.b32.ns <- length(daysbelow32[ghog=="NOShadow"])
+ln.b32.s <- length(daysbelow32[ghog=="Shadow"])
 
+mean.b32.s
+ttest.s <- (mean.b32.ns-mean.b32.s)/sqrt((sd.b32.ns^2/ln.b32.ns) + (sd.b32.s^2/ln.b32.s))
 
+ttest.s
+#t test with alternative not equal
+t.test(daysbelow32[ghog=="NOShadow"],daysbelow32[ghog=="Shadow"],alternative=c("greater"))
 
+#t test with alternative greater
+t.test(daysbelow32[ghog=="NOShadow"],daysbelow32[ghog=="Shadow"],alternative=c("greater"))
+
+pt(-.3112,20.11)
